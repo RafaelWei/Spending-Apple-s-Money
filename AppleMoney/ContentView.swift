109ea1be8f68a49b79da3b8b0aabeac7b9a2ba36
rafaelwei <rafaelwei07@gmail.com>
2020-09-28 21:54:03 -0300

    Initial Commit

---
+++ AppleMoney/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleMoney
+//
+//  Created by Rafael Wei on 28/09/20.
+//  Copyright © 2020 Rafael Wei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
